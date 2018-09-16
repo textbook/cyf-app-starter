a88db1dc54dd5e1a9c08f2358cb726007a51d21e
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-09-16 18:36:35 +0100

    Tolerate failure of query execution
    If the query wasn't executed, you can't destructure rows
    from the second argument to the callback (as it will be
    null or undefined).

--- server/routes.js
+++ server/routes.js
@@ -5,11 +5,11 @@ const db = require('./database');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM data;', (err, { rows }) => {
+  db.query('SELECT * FROM data;', (err, data) => {
     if (err) {
       return res.sendStatus(500);
     }
-    res.send(rows[0]);
+    res.send(data.rows[0]);
   });
 });
 
