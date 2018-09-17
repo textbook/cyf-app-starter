618ba98ace7c72a78c9a0849ac4c9a9abc1493e6
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-09-17 20:54:42 +0100

    Add more testing to the client

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import TestRenderer from 'react-test-renderer';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  fetch.mockResponseOnce(JSON.stringify({ message: 'Hello, world!' }));
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  const message = 'Hello, world!';
+
+  beforeEach(() => {
+    fetch.resetMocks();
+    fetch.mockResponseOnce(JSON.stringify({ message }));
+  });
+
+  it('renders without crashing', () => {
+    expect(TestRenderer.create(<App />).toJSON()).toMatchSnapshot();
+  });
+
+  it('displays the message from the server', done => {
+    const app = TestRenderer.create(<App />);
+    setTimeout(() => {
+      expect(app.root.findByType('h2').children).toContain(message);
+      done();
+    });
+  });
 });
